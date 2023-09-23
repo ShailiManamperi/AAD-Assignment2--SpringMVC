2e45b21af0fb3f3399f36d53651f5b8f6ff226df
Shaili Manamperi <shailimanamperi2002@gmail.com>
2023-09-23 08:16:09 +0530

    Add  data to endpoint package files

--- src/main/java/lk/ijse/aad/Endpoints/ProjectController.java
+++ src/main/java/lk/ijse/aad/Endpoints/ProjectController.java
@@ -1,12 +1,42 @@
 package lk.ijse.aad.Endpoints;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.aad.Dto.ProjectDTO;
+import lk.ijse.aad.Responce.Response;
+import lk.ijse.aad.Service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/project")
 @CrossOrigin
 public class ProjectController {
+    @Autowired
+    private ProjectService projectsService;
+
+    @PostMapping(path = "/save", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Response save(@RequestBody ProjectDTO projectsDto) {
+        return projectsService.save(projectsDto);
+    }
+
+    @PutMapping(path = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response update(@RequestBody ProjectDTO projectsDto) {
+        return projectsService.update(projectsDto);
+    }
+
+    @GetMapping(path = "/search", params = "projectId", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response search(@RequestParam("projectId") String projectId) {
+        return projectsService.search(projectId);
+    }
+
+    @DeleteMapping(path = "/delete", params = "projectId", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response delete(@RequestParam("projectId") String projectId) {
+        return projectsService.delete(projectId);
+    }
+
+    @GetMapping(path = "/fetchAll", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response getAll() {
+        return projectsService.getAll();
+    }
 }
--- src/main/java/lk/ijse/aad/Endpoints/TechLeadController.java
+++ src/main/java/lk/ijse/aad/Endpoints/TechLeadController.java
@@ -1,11 +1,41 @@
 package lk.ijse.aad.Endpoints;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.aad.Dto.TechLeadDTO;
+import lk.ijse.aad.Responce.Response;
+import lk.ijse.aad.Service.TechLeadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/project")
 @CrossOrigin
 public class TechLeadController {
+    @Autowired
+    private TechLeadService techLeadService;
+
+    @PostMapping(path = "/save", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response save(@RequestBody TechLeadDTO techLead_dto) {
+        return techLeadService.save(techLead_dto);
+    }
+
+    @PutMapping(path = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response update(@RequestBody TechLeadDTO techLead_dto) {
+        return techLeadService.update(techLead_dto);
+    }
+
+    @GetMapping(path = "/search", params = "techLeadId", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response search(@RequestParam("techLeadId") String techLeadId) {
+        return techLeadService.search(techLeadId);
+    }
+
+    @DeleteMapping(path = "/delete", params = "techLeadId", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response delete(@RequestParam("techLeadId") String techLeadId) {
+        return techLeadService.delete(techLeadId);
+    }
+
+    @GetMapping(path = "/fetchAll", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response getAll() {
+        return techLeadService.getAll();
+    }
 }
