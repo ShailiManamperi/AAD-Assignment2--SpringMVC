2a2c55f25824a0b09ae30b10d018cd66c5d3cab8
Shaili Manamperi <shailimanamperi2002@gmail.com>
2023-09-23 08:32:29 +0530

    update controller file

--- src/main/java/lk/ijse/aad/Endpoints/TechLeadController.java
+++ src/main/java/lk/ijse/aad/Endpoints/TechLeadController.java
@@ -8,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/project")
+@RequestMapping("/Tech")
 @CrossOrigin
 public class TechLeadController {
     @Autowired
