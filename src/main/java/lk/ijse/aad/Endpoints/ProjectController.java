d37f6f1597d36856b07b84f7f0e6d0b71f6746aa
Shaili Manamperi <shailimanamperi2002@gmail.com>
2023-09-23 08:05:58 +0530

    Add files and data in endpoint package

--- src/main/java/lk/ijse/aad/Endpoints/ProjectController.java
+++ src/main/java/lk/ijse/aad/Endpoints/ProjectController.java
@@ -1,4 +1,12 @@
 package lk.ijse.aad.Endpoints;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/project")
+@CrossOrigin
 public class ProjectController {
 }
--- src/main/java/lk/ijse/aad/Endpoints/TechLeadController.java
+++ src/main/java/lk/ijse/aad/Endpoints/TechLeadController.java
@@ -1,4 +1,11 @@
 package lk.ijse.aad.Endpoints;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/project")
+@CrossOrigin
 public class TechLeadController {
 }
